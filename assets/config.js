f74d0b880da16551607ae3b50969e878c41d0abf
THIEN-BAN <thien-ban@THIEN-BANs-MacBook-Air.local>
2026-08-08 12:13:43 +0700

    refactor: nâng cấp cấu trúc 3 tầng (Tổ chức/Chương trình/Môn học) cho LearnIZ

--- assets/config.js
+++ assets/config.js
@@ -2,29 +2,33 @@ window.TVU_SUBJECTS = [
   {
     id: "10",
     name: "Nguyên lý kế toán",
-    program: "Quản trị kinh doanh",
+    organization: "TVU",
+    program: "Quản trị kinh doanh (Đại học)",
     desc: "Nắm vững quy trình ghi chép và lập báo cáo tài chính.",
-    jsonPath: "./_sources/Quan_tri_kinh_doanh/10. Nguyên lý kế toán/Ngan_hang_de_Nguyen_ly_ke_toan_qs.json"
+    jsonPath: "./_sources/TVU/Quan_tri_kinh_doanh_DH/10. Nguyên lý kế toán/Ngan_hang_de_Nguyen_ly_ke_toan_qs.json"
   },
   {
     id: "11",
     name: "Kinh tế chính trị Mác - Lênin",
-    program: "Quản trị kinh doanh",
+    organization: "TVU",
+    program: "Quản trị kinh doanh (Đại học)",
     desc: "Bản chất quy luật kinh tế và giá trị thặng dư.",
-    jsonPath: "./_sources/Quan_tri_kinh_doanh/11. Kinh tế chính trị Mác - Lênin/Ngan_hang_de_Mac_Lenin_qs.json"
+    jsonPath: "./_sources/TVU/Quan_tri_kinh_doanh_DH/11. Kinh tế chính trị Mác - Lênin/Ngan_hang_de_Mac_Lenin_qs.json"
   },
   {
     id: "12",
     name: "Thống kê ứng dụng trong kinh doanh",
-    program: "Quản trị kinh doanh",
+    organization: "TVU",
+    program: "Quản trị kinh doanh (Đại học)",
     desc: "Công cụ phân tích, thu thập và dự báo dữ liệu kinh doanh.",
-    jsonPath: "./_sources/Quan_tri_kinh_doanh/12. Thống kê ứng dụng trong kinh doanh/Ngan_hang_de_Thong_ke_qs.json"
+    jsonPath: "./_sources/TVU/Quan_tri_kinh_doanh_DH/12. Thống kê ứng dụng trong kinh doanh/Ngan_hang_de_Thong_ke_qs.json"
   },
   {
     id: "13",
     name: "Giáo Dục Thể Chất 1",
-    program: "Quản trị kinh doanh",
+    organization: "TVU",
+    program: "Quản trị kinh doanh (Đại học)",
     desc: "Môn học giáo dục thể chất cơ bản.",
-    jsonPath: "./_sources/Quan_tri_kinh_doanh/13. GIÁO DỤC THỂ CHẤT 1/qs.json"
+    jsonPath: "./_sources/TVU/Quan_tri_kinh_doanh_DH/13. GIÁO DỤC THỂ CHẤT 1/qs.json"
   }
 ];
